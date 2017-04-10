25bee5196c165021b0a452c14ac8e87903e4b3e6
Maali Hasan <mhasan@asaltech.com>
2017-04-10 10:09:54 +0300

    pass two number to activity two ,then read them  ,return the summation to first activity

--- app/src/main/java/com/example/mhasan/multipleactivities/ActivityTwo.java
+++ app/src/main/java/com/example/mhasan/multipleactivities/ActivityTwo.java
@@ -1,5 +1,6 @@
 package com.example.mhasan.multipleactivities;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,29 +8,37 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class ActivityTwo extends AppCompatActivity implements View.OnClickListener {
+public class ActivityTwo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);// retrieve a reference to actionbar
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_two);
-        init();
-    }
 
-    void init() {
-        Button btn2 = (Button) findViewById(R.id.CoolingActivity1);
-        btn2.setOnClickListener(this);
-
-        String  FirstNum= getIntent().getExtras().getString("FirstNum");
-        String  ScondNum=getIntent().getExtras().getString("ScondNum");
-        Log.d("We got the first Number",FirstNum);
-        Log.d("We got the scond Number",ScondNum);
     }
 
     @Override
-    public void onClick(View v) {
-        Intent getFirstActivity = new Intent(this, MainActivity.class);
-        startActivity(getFirstActivity);
-
+    public boolean onSupportNavigateUp() {
+        setData();
+        finish();
+        return true;
     }
+
+    public void setData() {
+        String FirstNum = getIntent().getExtras().getString("FirstNum");
+        String ScondNum = getIntent().getExtras().getString("ScondNum");
+
+        int result = Integer.parseInt(FirstNum) + Integer.parseInt(ScondNum);
+        Intent getFirstActivity = new Intent();
+        getFirstActivity.putExtra("result", String.valueOf(result));
+        setResult(Activity.RESULT_OK, getFirstActivity);
+        finish();
+        Log.d("The result", String.valueOf(result));
+    }
+
+
 }
--- app/src/main/java/com/example/mhasan/multipleactivities/MainActivity.java
+++ app/src/main/java/com/example/mhasan/multipleactivities/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.mhasan.multipleactivities;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText ScndNum, firstNum;
@@ -13,7 +15,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_main);
+
         init();
     }
 
@@ -21,19 +27,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button btn1 = (Button) findViewById(R.id.CoolingActivity2);
         firstNum = (EditText) findViewById(R.id.firstNum);
         ScndNum = (EditText) findViewById(R.id.ScndNum);
-
         btn1.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
+        final int code = 1;
         String Num1 = firstNum.getText().toString();
         String Num2 = ScndNum.getText().toString();
 
-
         Intent getSecondActivity = new Intent(this, ActivityTwo.class);
-        getSecondActivity.putExtra("FirstNum",Num1);
-        getSecondActivity.putExtra("ScondNum",Num2);
-        startActivity(getSecondActivity);
+        getSecondActivity.putExtra("FirstNum", Num1);
+        getSecondActivity.putExtra("ScondNum", Num2);
+        startActivityForResult(getSecondActivity, code);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                String dataResult=data.getStringExtra("result");
+                TextView result = (TextView) findViewById(R.id.result);
+                result.setText( dataResult);
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+
+            }
+        }
+
+    }
+
+
 }
